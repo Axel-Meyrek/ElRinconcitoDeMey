79f0421baf4a7a3c9587c10d44a2a891de2b5912
Axel Meyrek Mac <chavezsud1406@gmail.com>
2026-01-22 21:29:23 -0600

    Navegacion

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log("Hola mundo");
+/* VARIABLES Y COMPONENTES */
+let buttonsNavegacion = [];
+
+let secciones = [];
+
+
+
+
+/* FUNCIONES */
+const recuperarBotonesDeNavegacion = () => buttonsNavegacion = document.querySelectorAll('[data-modal]');
+
+const recuperarSecciones = () => secciones = document.querySelectorAll('.windowFloat');
+
+const agregarEventoABotonesNavegacion = () => {
+    buttonsNavegacion.forEach( (button) => {
+        button.addEventListener('click', (e) => {
+            const seccionAMostrarID = e.currentTarget.getAttribute('data-modal');
+
+            secciones.forEach( (seccion) => {
+                if(seccion.id === seccionAMostrarID){
+                    seccion.classList.add('showWindowFloat');
+                } else {
+                    seccion.classList.remove('showWindowFloat');
+                }
+            });
+        });
+    });
+}
+
+/* EVENTOS */
+document.addEventListener('DOMContentLoaded', () => {
+    
+    recuperarBotonesDeNavegacion();
+    
+    recuperarSecciones();
+
+    agregarEventoABotonesNavegacion();
+    
+});
